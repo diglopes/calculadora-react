a5c445824702e8b46965aa429cfcb8ac5c71eebb
Diego Lopes <diego-chez@hotmail.com>
2019-04-26 21:40:37 -0300

    feat: add conditional style classes to button component

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -8,23 +8,23 @@ export default class Calculator extends Component {
     return (
       <div className="calculator">
         <Display value={100} />
-        <Button label="AC" />
-        <Button label="/" />
+        <Button label="AC" triple={true} />
+        <Button label="/" operation={true} />
         <Button label="7" />
         <Button label="8" />
         <Button label="9" />
-        <Button label="*" />
+        <Button label="*" operation={true} />
         <Button label="4" />
         <Button label="5" />
         <Button label="6" />
-        <Button label="-" />
+        <Button label="-" operation={true} />
         <Button label="1" />
         <Button label="2" />
         <Button label="3" />
-        <Button label="+" />
-        <Button label="0" />
+        <Button label="+" operation={true} />
+        <Button label="0" double={true} />
         <Button label="." />
-        <Button label="=" />
+        <Button label="=" operation={true} />
       </div>
     );
   }
